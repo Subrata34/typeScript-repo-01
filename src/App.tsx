0a2003031ad789f0970cbcf67ec059ec7b2a6bd2
Subrata34 <subratamodak3491@gmail.com>
2022-02-25 00:35:09 +0600

    files

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,18 @@ import React from 'react';
 import './App.css';
 import Lists from "./Components/Lists";
 
+// const Box:React.FunctionComponent=({children})=>{
+
+//   return <div>{children}</div>
+// };
+
+const Box:React.FunctionComponent<{title:string}>=({title})=>{
+  return <div></div>;
+};
 function App() {
   return (
     <div className="App">
+      <Box title="hallo">sdaklfjkaslj</Box>
       <Lists></Lists>
     </div>
   );
